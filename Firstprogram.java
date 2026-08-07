129f0c1c5b191fa74633ddbe0b8a4636b8ab010a
ibrahimxcode <ibrahimfakir692@gmail.com>
2026-08-08 00:51:20 +0530

    added first java program file

---
+++ Firstprogram.java
@@ -0,0 +1,6 @@
+// Save this file exactly as Main.java
+public class Firstprogram {
+    public static void main(String[] args) {
+        System.out.println("Hellow World ! This is Ibrahim");
+    }
+}
