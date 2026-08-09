9abe6e042f238b12c345faa14b9cd9e7d83f5630
ibrahimxcode <ibrahimfakir692@gmail.com>
2026-08-09 18:10:44 +0530

    Added Practice program 1. simple calculator

---
+++ 07_PracticeProgram's/Simplecalculator.java
@@ -0,0 +1,15 @@
+public class Simplecalculator {
+        public static void main(String[] args)
+    {
+        // first we declear two values to perform all this operators 
+        int A = 10;
+        int B = 5;
+        
+        System.out.printf("Result = %d%n",A+B);//Addition of A + B
+        System.out.printf("Result = %d%n",A-B);//Subtraction of A -B
+        System.out.printf("Result = %d%n",A*B);//Multiplication of A * B
+        System.out.printf("Result = %d%n",A/B);//Division of A / B
+        System.out.printf("Result = %d%n",A%B);//Remainder of A % B
+    }
+    
+}
