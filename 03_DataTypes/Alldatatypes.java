31559058eabb9a6612c04938134b896681ac4e28
ibrahimxcode <ibrahimfakir692@gmail.com>
2026-08-09 15:58:41 +0530

    Added All Data types Files

---
+++ 03_DataTypes/Alldatatypes.java
@@ -0,0 +1,30 @@
+public class Alldatatypes {
+    public static void main(String[] args)
+    {
+        // Here are all primitive Data types
+        byte age = 18;
+        short year = 26;
+        int marks = 85;
+        long population = 8307500000L;
+        float price = 99.99f;
+        double pi = 3.14159;
+        char grade = 'A';
+        boolean passed = true;
+        String name = "ibrahim"; // This is Non-primitive/referce Data type 
+
+        // Now here we going to print all variabls by their format spacifier
+        System.out.println("=====================All Data Types====================");
+        System.out.println("age =" + age );
+        System.out.println("years =" + year);
+        System.out.println("marks =" + marks);
+        System.out.println("population = " + population);
+        System.out.println("price =" + price);
+        System.out.println("pi =" + pi);
+        System.out.println("grade =" + grade);
+        System.out.println("passed =" + passed);
+        System.out.println("name =" + name );
+
+
+    }
+    
+}
