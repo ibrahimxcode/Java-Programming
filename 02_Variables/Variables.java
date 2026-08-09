36679fac6070ccd1ef6ce0b758926a5e4ddc1bd4
ibrahimxcode <ibrahimfakir692@gmail.com>
2026-08-09 15:22:16 +0530

    Added Variable use file

---
+++ 02_Variables/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args)
+    {
+        /* 
+        Here we useing Variable to print Date/Month/year by using variable's
+        */
+        int Variable_date = 9;      
+        int Variable_month = 8;
+        int Variable_year = 2026;
+        
+        //here going to print all those values
+        System.out.printf("%d/%d/%d", Variable_date, Variable_month, Variable_year);
+    }
+    
+}
